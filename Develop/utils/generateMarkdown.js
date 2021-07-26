41c2de9d8e29634aa53f43379448453cfa61d99c
AngelDragoniusX <dialga1994@gmail.com>
2021-07-26 02:04:52 -0400

    upload file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,121 +1,93 @@
-/ TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (!license) {
-    return '';
-  } else if (license) {
-      if (license == 'GNU AGPLv3') {
-        return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
-      } else if (license == 'GNU GPLv3') {
-        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-      } else if (license == 'GNU LGPLv3') {
-        return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`
-      } else if (license == 'Mozilla Public License 2.0') {
-        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
-      } else if (license == 'Apache License 2.0') {
-        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-      } else if (license == 'MIT License') {
-        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-      } else if (license == 'Boost Software License 1.0') {
-        return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
-      } else if (license == 'ISC License') {
-        return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
-      } else if (license == 'The Unlicense') {
-        return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`  
-    }
-  }
-};
+  const url = encodeURI(`https://img.shields.io/badge/license-${license}-blue.svg`);
+  return `![${license}](${url})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!license) {
-    return '';
-  } else if (license) {
-    for (i = 0; i < 1; i++) {
-      if (license == 'GNU AGPLv3') {
-        return `
-        https://www.gnu.org/licenses/agpl-3.0
-        `
-      } else if (license == 'GNU GPLv3') {
-        return `
-        https://www.gnu.org/licenses/gpl-3.0
-        `
-      } else if (license == 'GNU LGPLv3') {
-        return `
-        https://www.gnu.org/licenses/lgpl-3.0
-        `
-      } else if (license == 'Mozilla Public License 2.0') {
-        return `
-        https://opensource.org/licenses/MPL-2.0
-        `
-      } else if (license == 'Apache License 2.0') {
-        return `
-        https://opensource.org/licenses/Apache-2.0
-        `
-      } else if (license == 'MIT License') {
-        return `
-        https://opensource.org/licenses/MIT
-        `
-      } else if (license == 'Boost Software License 1.0') {
-        return `
-        https://www.boost.org/LICENSE_1_0.txt
-        `
-      } else if (license == 'ISC License') {
-        return `
-        https://opensource.org/licenses/IS
-        `
-      } else if (license == 'The Unlicense') {
-        return `
-        http://unlicense.org/
-        `
-      }
-    }
+  const licenses = {
+    ['GNU AGPLv3']: 'https://choosealicense.com/licenses/agpl-3.0/',
+    ['GNU GPLv3']: 'https://choosealicense.com/licenses/gpl-3.0/',
+    ['GNU LGPLv3']: 'https://choosealicense.com/licenses/lgpl-3.0/',
+    ['Mozilla Public License 2.0']: 'https://choosealicense.com/licenses/mpl-2.0/',
+    ['Apache License 2.0']: 'https://choosealicense.com/licenses/apache-2.0/',
+    ['MIT License']: 'https://choosealicense.com/licenses/mit/',
+    ['Boost Software License 1.0']: 'https://choosealicense.com/licenses/bsl-1.0/',
+    ['The Unlicense']: 'https://choosealicense.com/licenses/unlicense/',
+    ['']: ''
   }
-};
+  return licenses[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license) {
-    return '';
-  } else if (license) {
-    for (i = 0; i < 1; i++) {
-    return `## **_License_**
-    
-This project is covered under the ${license} license.
-Click [here](${renderLicenseLink(license)}) to access more information on the license.`
-}
+  const licenses = {
+    ['GNU AGPLv3']: `Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+    Everyone is permitted to copy and distribute verbatim copies
+    of this license document, but changing it is not allowed.`,
+    ['GNU GPLv3']: `Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+    Everyone is permitted to copy and distribute verbatim copies
+    of this license document, but changing it is not allowed.`,
+    ['GNU LGPLv3']: `Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+    Everyone is permitted to copy and distribute verbatim copies
+    of this license document, but changing it is not allowed.`,
+    ['Mozilla Public License 2.0']: `This License is not intended to limit any rights You have under
+    applicable copyright doctrines of fair use, fair dealing, or other
+    equivalents.`,
+    ['Apache License 2.0']: `Copyright 2021 Landa`,
+    ['MIT License']: `Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:`,
+    ['Boost Software License 1.0']: `Permission is hereby granted, free of charge, to any person or organization
+    obtaining a copy of the software and accompanying documentation covered by
+    this license (the "Software") to use, reproduce, display, distribute,
+    execute, and transmit the Software, and to prepare derivative works of the
+    Software, and to permit third-parties to whom the Software is furnished to
+    do so, all subject to the following:`,
+    ['The Unlicense']: ``,
+    ['']: ''
   }
-};
+  return licenses[license];
+}
 
 // TODO: Create a function to generate markdown for README
-module.exports = (readmeInputs, license) => {
-  console.log(readmeInputs, license);
-  return ` # **_${readmeInputs.projectName}_**
-  ${renderLicenseBadge(readmeInputs.license)}
-  ## **_Description_**
-  ${readmeInputs.projectDescription}
-  ## **_Table of contents_**
-  * [Description](#description)
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [License](#license)
-  * [Questions](#questions)
-    
-  ## **_Installation_**
-  ${readmeInputs.installationInst}
-  ## **_Usage_**
-  ${readmeInputs.usageInst}
-  ## **_Contributing_**
-  ${readmeInputs.credits}
-  ## **_Tests_**
-  ${readmeInputs.testCases}
-${renderLicenseSection(readmeInputs.license)}
-  ## **_Questions_**
-  Please visit [my GitHub](https://github.com/${readmeInputs.githubUsername})
-  and if you're still scratching your head over this, drop me a line here [email address](mailto:${readmeInputs.email}) and I'll reach out ASAP.
-  `;
-};
+function generateMarkdown(data) {
+  return `# **${data.title}**\n
+>## **_Description_**\n
+${data.description}\n
+---
+### **_Table of Contents_**\n
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Features](#features)
+* [Contribution](#contribution)
+* [License](#license)
+>## **_Installation_**\n
+\`\`\`
+${data.installation}
+\`\`\`\n
+>## **_Usage_**\n
+${data.usage}\n
+>## **_Credits_**\n
+${data.credits}\n
+>## **_Features_**\n
+${data.features}\n
+>## **_Contributing_**\n
+If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.\n
+${data.contribution}\n
+>## **_License_**\n
+${renderLicenseBadge(data.license)}\n
+${renderLicenseSection(data.license)}\n
+${renderLicenseLink(data.license)}\n
+`;
+}
+
+module.exports = generateMarkdown;
